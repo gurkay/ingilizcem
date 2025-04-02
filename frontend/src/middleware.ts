52cd2448ab5a9fb66f0bbbbc3e0b7dde157a667d
gurkay <g_basyigit@hotmail.com>
2025-04-02 13:20:28 +0300

    test

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -7,7 +7,10 @@ export function middleware(req: NextRequest, event: NextFetchEvent) {
     console.log("middleware çalıştı");
     console.log("req.cookies", req.cookies);
     const token = req.cookies.get("next-auth.session-token");
+    const token2 = req.cookies.get("__Secure-next-auth.session-token");
     console.log("token", token);
+    console.log("token2", token2);
+
     if (!token?.value) {
       // Authentication failed
       return NextResponse.redirect(new URL("/auth/signin", req.url)); // Redirect to 
