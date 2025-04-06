752d7f38b824a0d993af0383ae71966ef834d938
gurkay <g_basyigit@hotmail.com>
2025-04-06 14:54:55 +0300

    test

--- backend/src/main/java/com/template/backend/controllers/LessonController.java
+++ backend/src/main/java/com/template/backend/controllers/LessonController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
-@RequestMapping("/api/lessons")
+@RequestMapping({"/api/lessons", "/lessons"})
 public class LessonController {
 
     @Autowired
