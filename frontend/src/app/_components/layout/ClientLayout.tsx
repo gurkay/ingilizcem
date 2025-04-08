5a3e21491aecbbff80a6209e8313d74bb3c4b936
gurkay <g_basyigit@hotmail.com>
2025-04-08 21:04:39 +0300

    test

--- frontend/src/app/_components/layout/ClientLayout.tsx
+++ frontend/src/app/_components/layout/ClientLayout.tsx
@@ -9,18 +9,24 @@ const callSignOut = async () => {
 };
 
 
-export function SignOutButton(){
-  return(
+export function SignOutButton() {
+  return (
     <button onClick={callSignOut} className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded">
-    Sign out
-  </button>
+      <svg className="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+      </svg>
+      Exit
+    </button>
   )
-}
+} 
 
-export function DashboardButton(){
-  return(
+export function DashboardButton() {
+  return (
     <Link href="/dashboard" className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
-    Dashboard
-  </Link>
+      <svg className="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+      </svg>
+      Home
+    </Link>
   )
 }
